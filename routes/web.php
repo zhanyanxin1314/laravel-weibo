aa3cb23ec1902f372627c0132d9328c0ff1e1673
zhangyanxin <18810570163@163.com>
2018-06-27 11:08:58 +0800

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
 Route::get('/signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
